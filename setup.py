78cab61a087495527f3c2501dda8b717de4c7768
kube <keem@KubeM1.local>
2024-09-03 11:19:19 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ansible-beam',
-    version='0.1.0',
+    version='1.0.0',
     description='A project to generate Ansible playbooks and roles using AI',
     author='BlueHat Keem',
     author_email='keem@kubeworld.io',
